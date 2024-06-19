6b55f68b04d73bf8419473d234969fc2195cb853
Vamshi Krishna <85406957+vamshi1905@users.noreply.github.com>
2024-06-19 15:36:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   appDir: true,
-  //   swcPlugins: [["next-superjson-plugin", {}]]
-  // },
+  experimental: {
+    appDir: true,
+    // swcPlugins: [["next-superjson-plugin", {}]]
+  },
   images: {
     domains: [
       'res.cloudinary.com', 
