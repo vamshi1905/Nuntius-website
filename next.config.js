3e37fda6c41da1284308ff6cd7546976425d9089
Vamshi Krishna <85406957+vamshi1905@users.noreply.github.com>
2024-06-19 15:39:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    // swcPlugins: [["next-superjson-plugin", {}]]
+    swcPlugins: [["next-superjson-plugin", {}]]
   },
   images: {
     domains: [
